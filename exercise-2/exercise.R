5745b79ef7d41a556af9f200b5a96c48a00a7ecd
Ott Toomet <otoomet@uw.edu>
2021-04-12 21:22:42 -0700

    ex2: all and any master

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,10 +1,34 @@
-# Exercise 2: writing and executing functions (II)
+## Exercise 2:  vector functions
+
+### ---------- any and all ----------
+### These are very useful utility functions for coding
+
+
+## Write a function that takes a vector of numbers as argument
+## and tells if all numbers are even
+## Hint: use modulo operator %% and check out 'all' function
+
+
+## Pass a vector to test if it works correctly
+
+
+## Write a function that takes a vector of numbers as argument
+## and tells if among all the numbers there is at least one
+## odd number
+## Hint: check out 'any' function
+
+
+## Pass a vector to test if it works correctly
+
+
+### ---------- vector length ----------
 
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
 
 
-# Pass two vectors of different length to your `CompareLength` function
+## Pass two vectors of different length to your `CompareLength` function
+## to check if it works as intended 
 
 
 # Write a function `DescribeDifference` that will return one of the following statements:
@@ -12,9 +36,6 @@
 # "Your second vector is longer by N elements"
 
 
-# Pass two vectors to your `DescribeDifference` function
+## Pass two vectors to your `DescribeDifference` function
+## check if it works correctly
 
-
-### Bonus ###
-
-# Rewrite your `DescribeDifference` function to tell you the name of the vector which is longer
