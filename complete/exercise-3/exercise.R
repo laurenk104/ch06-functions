e18b0155d4f4e00c7bdceb9fc86a567c764afa73
Ott Toomet <otoomet@uw.edu>
2021-04-12 21:49:38 -0700

    ch6 exercises overlooked, added some ex3 stuff for string replace

--- complete/exercise-3/exercise.R
+++ complete/exercise-3/exercise.R
@@ -9,10 +9,24 @@
 ##   "Your strings are the same length!"
 ##
 ## Hint: check out function 'nchar'
+compareStrings <- function(s1, s2) {
+   l1 <- nchar(s1)
+   l2 <- nchar(s2)
+   if(l1 > l2) {
+      res <- paste("your first string is longer by", l1-l2, "characters")
+   } else if(l2 > l1) {
+      res <- paste("your second string is longer by", l2-l1, "characters")
+   } else
+      res <- "Your strings are of the same length!"
+   return(res)
+}
 
 
 ## Call your `describe_difference` function by passing it different length strings
 ## to confirm that it works. Make sure to check all 3 conditions1
+compareStrings("abd", "dddd")
+compareStrings("abduuss", "dddd")
+compareStrings("abduuss", "ddddddd")
 
 
 
@@ -23,11 +37,27 @@
 ## 
 ## Hint: compare the length difference to the length of the smaller string
 ## 
+twiceLong <- function(s1, s2) {
+   ## compute lengths
+   l1 <- nchar(s1)
+   l2 <- nchar(s2)
+   if(l1 < l2) {
+      ## s1 is shorter: the difference must be equal to l1
+      return(l2 - l1 == l1)
+   }
+   ## that was not true: hence l2 shorter, the difference mus
+   ## equal to l2
+   return(l1 - l2 == l2)
+}
 
 
 # Call your `twiceLong` function by passing it different length strings
 # to confirm that it works. Make sure to check when _either_ argument is twice
 # as long, as well as when neither are!
+twiceLong("abc", "aaggdd")
+twiceLong("abc", "aaggd")
+twiceLong("aaggd", "abc")
+twiceLong("aaggdd", "abc")
 
 
 
@@ -37,8 +67,18 @@
 ## You can add more words in this list.
 ## The function should return the same sentence, just all expletives replaced
 ## by "***".
+censor <- function(s) {
+   ## Do with a for-loop.
+   ## If students do not know the loop, can also just put the gsub
+   ## functions underneath each other.
+   expletives <- c("moron", "damned")
+   for(expletive in expletives) {
+      s <- gsub(expletive, "***", s)
+   }
+   s
+}
 
 
 ## Call your function with a sentency that contains expletives.
 ## Show that it works.
-
+censor("You are a damned moron!")
