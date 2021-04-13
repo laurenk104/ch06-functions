067b61cdf9f563c448d9347a5d1d4a90f6d54855
Ott Toomet <otoomet@uw.edu>
2021-04-12 21:21:37 -0700

    ex2: all and any

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,4 +1,32 @@
-# Exercise 2: writing and executing functions (II)
+## Exercise 2:  vector functions
+
+### ---------- any and all ----------
+### These are very useful utility functions for coding
+
+## Write a function that takes a vector of numbers as argument
+## and tells if all numbers are even
+## Hint: use modulo operator %% and check out 'all' function
+allEven <- function(x) {
+   all(x %% 2 == 0)
+}
+
+## Pass a vector to test if it works correctly
+allEven(c(2,4,6))
+allEven(c(2,5,4))
+
+## Write a function that takes a vector of numbers as argument
+## and tells if among all the numbers there is at least one
+## odd number
+## Hint: check out 'any' function
+anyOdd <- function(x) {
+   any(x %% 2 == 1)
+}
+
+## Pass a vector to test if it works correctly
+anyOdd(c(2,4,6))
+anyOdd(c(2,5,4))
+
+### ---------- vector length ----------
 
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
@@ -8,7 +36,8 @@ CompareLength <- function(v1, v2) {
   return(diff.string)
 }  
 
-# Pass two vectors of different length to your `CompareLength` function
+## Pass two vectors of different length to your `CompareLength` function
+## to check if it works as intended 
 CompareLength(c(1,2,3), 1:100)
 
 # Write a function `DescribeDifference` that will return one of the following statements:
@@ -24,24 +53,6 @@ DescribeDifference <- function(a,b) {
   return(sentence)
 }
 
-# Pass two vectors to your `DescribeDifference` function
+## Pass two vectors to your `DescribeDifference` function
+## check if it works correctly
 DescribeDifference(1:15, 9:17)
-
-### Bonus ###
-
-# Rewrite your `DescribeDifference` function to tell you the name of the vector which is longer
-# This helps: http://stackoverflow.com/questions/5754367/using-substitute-to-get-argument-name-with
-DescribeDifference <- function(a,b) {
-  diff <- length(a) - length(b)
-  if(diff > 0) {
-    vector.name <- deparse(substitute(a))
-    sentence <- paste('Vector', vector.name, 'is longer by', diff, 'elements')
-  } else {
-    vector.name <- deparse(substitute(b))
-    sentence <- paste('Vector', vector.name, 'is longer by', -diff, 'elements')
-  }
-  return(sentence)
-}
-x <- 1:15
-y <- 9:170
-DescribeDifference(x, y)
